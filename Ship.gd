c9046bc3d10fc992e473d3ce7d65bb93db74dfcd
Mark Odell <m4rk.odell@gmail.com>
2019-07-18 21:21:18 -0500

    Super boost and thruster dampening

--- Ship.gd
+++ Ship.gd
@@ -1,6 +1,6 @@
 extends RigidBody2D
 
-export var thruster_magnitude = 350
+export var thruster_magnitude = 100
 export var rthruster_magnitude = 1000
 var screen_size
 
@@ -13,6 +13,9 @@ func _ready():
 func _integrate_forces(state):
 	if forwardInputOnly():
 		applied_force = thrust.rotated(rotation)
+		calculate_animation("forward")
+	elif Input.is_action_pressed("ui_up") && Input.is_action_pressed("ui_modify"):
+		applied_force = thrust.rotated(rotation) * 2
 		calculate_animation("forward")
 	else:
 		applied_force = Vector2()
@@ -36,6 +39,8 @@ func _integrate_forces(state):
 		$AnimatedSprite.flip_h = false
 	applied_torque = rotation_direction * rthruster_magnitude
 	
+	if modifyInputOnlyPressed():
+		apply_thruster_damping()
 	if applied_force.length() <= 0 && applied_torque == 0:
 		$AnimatedSprite.animation = "idle"
 		$AnimatedSprite.stop()
@@ -54,4 +59,20 @@ func calculate_animation(name):
 		$AnimatedSprite.animation = startName
 
 func forwardInputOnly():
-	return Input.is_action_pressed("ui_up") && !Input.is_action_pressed("ui_right") && !Input.is_action_pressed("ui_left")
+	return Input.is_action_pressed("ui_up") && !Input.is_action_pressed("ui_right") && !Input.is_action_pressed("ui_left") && !Input.is_action_pressed("ui_modify")
+
+func modifyInputOnlyPressed():
+	return Input.is_action_pressed("ui_modify") && !Input.is_action_pressed("ui_up") && !Input.is_action_pressed("ui_right") && !Input.is_action_pressed("ui_left")
+
+func opposite_torque():
+	return angular_velocity * -1 * rthruster_magnitude
+
+func opposite_force():
+	return linear_velocity * -1 * thruster_magnitude
+	
+func apply_thruster_damping():
+	if angular_velocity >= 0:
+		applied_torque = opposite_torque()
+	elif linear_velocity.length() != 0:
+		applied_force = opposite_force()
+		
